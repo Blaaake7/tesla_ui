dbed75149b5ccd24f4246022f3c0fd6287c2be3c
이영진 <110272076+Blaaake7@users.noreply.github.com>
2024-12-17 10:43:00 +0900

    Update opencvimageprovider.cpp

--- Tesla/opencvimageprovider.cpp
+++ Tesla/opencvimageprovider.cpp
@@ -1,11 +1,13 @@
 #include "opencvimageprovider.h"
 
+// 초기 이미지를 검정색으로 설정
 OpencvImageProvider::OpencvImageProvider(QObject *parent) : QObject(parent), QQuickImageProvider(QQuickImageProvider::Image)
 {
     image = QImage(200,200,QImage::Format_RGB32);
     image.fill(QColor("black"));
 }
 
+// QML에서 이 함수가 호출되면 현재 이미지를 반환하고, 이미지 크기를 조정
 QImage OpencvImageProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
 {
     Q_UNUSED(id);
@@ -20,6 +22,7 @@ QImage OpencvImageProvider::requestImage(const QString &id, QSize *size, const Q
     return image;
 }
 
+// 새로운 이미지가 유효하고 기존 이미지와 다를 경우, 이미지를 갱신하고 `imageChanged` signal를 발생
 void OpencvImageProvider::updateImage(const QImage &image)
 {
     if(!image.isNull() && this->image != image) {
